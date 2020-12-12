5d9795987b2b5f7d0873722f1bfd4991ebf9aea8
Sera Brocious <sera.brocious@gmail.com>
2020-12-12 03:36:17 -0500

    Added initial lm service

---
+++ PsychopompNX/Services/Nn_Lm_Impl.swift
@@ -0,0 +1,12 @@
+//
+//  Nn_Lm_Impl.swift
+//  PsychopompNX
+//
+//  Created by Sera Brocious on 12/12/20.
+//
+
+import Foundation
+
+class NnLm_ILogService_Impl: NnLm_ILogService {
+    override func initialize(_ _0: UInt64, _ _1: Pid) throws -> NnLm_ILogger { NnLm_ILogger() }
+}
